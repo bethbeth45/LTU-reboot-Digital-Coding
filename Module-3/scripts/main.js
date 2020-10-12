a9fa1c42f81de643d0ed3d52908e702bfdbaeaaf
bethbeth45 <69627948+bethbeth45@users.noreply.github.com>
2020-10-12 16:26:54 +0100

    adding to JS

---
+++ Module-3/scripts/main.js
@@ -0,0 +1,18 @@
+
+var numberOfSmarties = 40;
+
+var numberOfIceCreams = 30;
+
+var AlltheSweets = numberOfSmarties + numberOfIceCreams; // 70
+
+console.log( AlltheSweets); // 70
+
+/*
+
++ add
+- subtract
+/ divide
+* multiply
+
+*/
+
